9ffd3d6673862a8fd15ccf371395bdb794ca8d7c
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-20 14:26:08 +0000

    Added two boolean variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -12,25 +12,33 @@ namespace VariousTypes
             int score = 100;
 
             // Create variable char with different unicode values
-            char o = '\u2764'; // heart symbol
-            char p = '\u2620'; // skull symbol
-            char q = '\u2603'; // snowman symbol
+            char heart = '\u2764'; // heart symbol
+            char skull = '\u2620'; // skull symbol
+            char snowman = '\u2603'; // snowman symbol
 
             // Create real variables of different types
             float real1 = 42.564f;
             double real2 = 84.942;
             decimal real3 = 14.761m;
 
+            // Create two boolean variables
+            bool big = true;
+            big = 5 < 3;
+            bool small = false;
+            small = 2 > 1;
+
             // Display the values of the variables
             Console.WriteLine(age);
             Console.WriteLine(year);
             Console.WriteLine(score);
-            Console.WriteLine(o);
-            Console.WriteLine(p);
-            Console.WriteLine(q);
+            Console.WriteLine(heart);
+            Console.WriteLine(skull);
+            Console.WriteLine(snowman);
             Console.WriteLine(real1);
             Console.WriteLine(real2);
             Console.WriteLine(real3);
+            Console.WriteLine(big);
+            Console.WriteLine(small);
         }
     }
 }
