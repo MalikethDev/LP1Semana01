2c61fe00253a6567890f68180158c695e61634c9
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-20 13:23:44 +0000

    Added variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Create integer variables of different types
+            int a = 10;
+            uint b = 20U;
+            short c = 30;
+            ushort d = 40;
+            long e = 50L;
+            ulong f = 60UL;
+            byte g = 70;
+            sbyte h = 80;
+            char i = 'A';
+            float j = 3.1415f;
+            double k = 100;
+            decimal l = 110.5m;
+            bool m = true;
+            string n = "Hello";
+
+            // Display the values of the variables
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+            Console.WriteLine(m);
+            Console.WriteLine(n);
+        }
+    }
+}
