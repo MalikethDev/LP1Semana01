94ec1863659e251274abade031f5436a766a2637
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-20 13:58:00 +0000

    Changed first part of the code to show only int numbers

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -7,44 +7,30 @@ namespace VariousTypes
         static void Main(string[] args)
         {
            // Create integer variables of different types
-            int a = 10;
-            uint b = 20U;
-            short c = 30;
-            ushort d = 40;
-            long e = 50L;
-            ulong f = 60UL;
-            byte g = 70;
-            sbyte h = 80;
-            char i = 'A';
-            float j = 3.1415f;
-            double k = 100;
-            decimal l = 110.5m;
-            bool m = true;
-            string n = "Hello";
+            int age = 27;
+            int year = 2025;
+            int score = 100;
 
             // Create variable char with different unicode values
             char o = '\u2764'; // heart symbol
             char p = '\u2620'; // skull symbol
             char q = '\u2603'; // snowman symbol
 
+            // Create real variables of different types
+            float real1 = 42.564f;
+            double real2 = 84.942;
+            decimal real3 = 14.761m;
+
             // Display the values of the variables
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            Console.WriteLine(f);
-            Console.WriteLine(g);
-            Console.WriteLine(h);
-            Console.WriteLine(i);
-            Console.WriteLine(j);
-            Console.WriteLine(k);
-            Console.WriteLine(l);
-            Console.WriteLine(m);
-            Console.WriteLine(n);
+            Console.WriteLine(age);
+            Console.WriteLine(year);
+            Console.WriteLine(score);
             Console.WriteLine(o);
             Console.WriteLine(p);
             Console.WriteLine(q);
+            Console.WriteLine(real1);
+            Console.WriteLine(real2);
+            Console.WriteLine(real3);
         }
     }
 }
