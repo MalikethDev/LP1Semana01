70187b04d651ccf449c5ffdb8bf72dce008a4d45
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-20 12:51:39 +0000

    First commit

---
+++ MeuPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeuPrrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
