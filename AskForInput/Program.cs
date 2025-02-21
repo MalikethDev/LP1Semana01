4979bcf27fa60575b74add289af6893259b941f5
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-21 12:36:20 +0000

    Created AskForInput Solution

---
+++ AskForInput/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask the user for an integer number
+            Console.Write("Please enter an integer number: ");
+        }
+    }
+}
