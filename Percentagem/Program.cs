cec9b8858a9e49b7011bab72317297a482d342d1
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-21 14:38:08 +0000

    Exercicio pratico de aula

---
+++ Percentagem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            // Ask the user for 4 real numbers between 0 and 4
+            Console.WriteLine("Please enter 4 real numbers between 0 and 4:");
+
+            // Print the 4 numbers with two decimal spaces as percentage
+            for (int i = 0; i < 4; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                Console.WriteLine(number.ToString("P2").PadLeft(10));
+            }
+        }
+    }
+}
