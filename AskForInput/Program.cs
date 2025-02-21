361f6caa4b4105f87ca39cca3af062a2886d3ace
Rodrigo <rodrigogomes091997@gmail.com>
2025-02-21 12:47:06 +0000

    Created AskForInputSolution

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -8,6 +8,15 @@ namespace AskForInput
         {
             // Ask the user for an integer number
             Console.Write("Please enter an integer number: ");
+
+            // Convert the string into an int and store it in a variable
+            int number = int.Parse(Console.ReadLine());
+
+            // Ask the user for a real number
+            Console.Write("Please enter a real number: ");
+
+            // Convert the string into a float and store it in another variable
+            float realNumber = float.Parse(Console.ReadLine());
         }
     }
 }
